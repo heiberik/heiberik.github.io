06019309caa51954703ea00efdb77c46c28a87de
Heiberik <37657797+Heiberik@users.noreply.github.com>
2020-02-23 17:08:09 +0100

    lagt til punktum

--- src/komponenter/sider/Startside.js
+++ src/komponenter/sider/Startside.js
@@ -10,7 +10,7 @@ const Startside = () => {
 
             <div className="forside_tekst">
                 <h1 className="forside_tittel">Henrik Heiberg</h1>
-                <h1 className="forside_tittel">Informatiker og jurist</h1>
+                <h1 className="forside_tittel">Informatiker og jurist.</h1>
                 <p>Velkommen til min karrierefremmende webside.</p>
             </div>
         </>
